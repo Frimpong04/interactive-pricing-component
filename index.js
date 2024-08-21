df9761ac3c218471040fc36b57f4bd28fb7df9a0
Kwame Frimpong-Boateng <frimpong@Kwames-MacBook-Air.local>
2024-08-21 20:04:38 +0000

    add switch styles

--- index.js
+++ index.js
@@ -2,7 +2,17 @@
 // slider element
 const sliderEl = document.querySelector("#slider");
 
+const numberOfViews = document.querySelector(".views-number");
+const amountDue = document.querySelector(".amount");
+
+// toggle buttons
+const monthlyBtn = document.querySelector(".monthly-btn");
+const yearlyBtn = document.querySelector(".yearly-btn");
+
 sliderEl.addEventListener("input", (event) => {
     const sliderValue = event.target.value ;
     sliderEl.style.background = `linear-gradient(to right, hsl(174, 77%, 80%) ${sliderValue}% , hsl(224, 65%, 95%) ${sliderValue}%  )`; 
+
+    numberOfViews.textContent = 100 * sliderValue;
+    amountDue.textContent = 16 * sliderValue;
 })
