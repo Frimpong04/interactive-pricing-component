2c61da0370f0daf2bf107ac9874ac034ef105bc7
Kwame Frimpong-Boateng <frimpong@Kwames-MacBook-Air.local>
2024-08-22 16:39:20 +0000

    add logic to the amount based on the duration

--- index.js
+++ index.js
@@ -6,13 +6,47 @@ const numberOfViews = document.querySelector(".views-number");
 const amountDue = document.querySelector(".amount");
 
 // toggle buttons
-const monthlyBtn = document.querySelector(".monthly-btn");
-const yearlyBtn = document.querySelector(".yearly-btn");
+const toggleEl = document.querySelector("#check-box");
+const duration = document.querySelector(".per-duration");
+
+
+let timeFrame = 12;
+let startAmount;
+
+toggleEl.addEventListener("change", (e) => {
+   console.log(e.target.checked);
+   if(e.target.checked) {
+    duration.textContent ="year";
+    timeFrame = 12;
+    startAmount = 16 * 12;
+    amountDue.textContent = startAmount;
+    console.log(timeFrame, startAmount);
+
+   } else {
+    duration.textContent = "month";
+    timeFrame = 1;
+    startAmount = 16
+    amountDue.textContent = startAmount;
+    console.log(timeFrame, startAmount);
+   }
+});
+
+
 
 sliderEl.addEventListener("input", (event) => {
     const sliderValue = event.target.value ;
     sliderEl.style.background = `linear-gradient(to right, hsl(174, 77%, 80%) ${sliderValue}% , hsl(224, 65%, 95%) ${sliderValue}%  )`; 
-
     numberOfViews.textContent = 100 * sliderValue;
-    amountDue.textContent = 16 * sliderValue;
+
+    amountDue.textContent = startAmount * timeFrame * sliderValue;
+    // if(!duration.checked) {
+    //     duration.textContent = "month";
+    //     amountDue.textContent = 16 * sliderValue;
+    // } else {
+    //     duration.textContent ="year";
+    //     amountDue.textContent = 16 * 12 * sliderValue;
+    // }
+
+
+   
 })
