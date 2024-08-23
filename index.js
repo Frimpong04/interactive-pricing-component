eb3b59f5f83d9b535c65de41ceeef626a077f3a7
Kwame Frimpong-Boateng <frimpong@Kwames-MacBook-Air.local>
2024-08-23 22:00:55 +0000

    add slider reset code

--- index.js
+++ index.js
@@ -11,7 +11,8 @@ const duration = document.querySelector(".per-duration");
 
 
 let timeFrame = 12;
-let startAmount;
+let startAmount = 16;
+let currentAmout;
 
 toggleEl.addEventListener("change", (e) => {
    console.log(e.target.checked);
@@ -20,13 +21,20 @@ toggleEl.addEventListener("change", (e) => {
     timeFrame = 12;
     startAmount = 16 * 12;
     amountDue.textContent = startAmount;
+    sliderEl.value=1;
+    numberOfViews.textContent = 100;
+    sliderEl.style.background = 'hsl(224, 65%, 95%)';
+   
     console.log(timeFrame, startAmount);
 
    } else {
     duration.textContent = "month";
     timeFrame = 1;
-    startAmount = 16
+    startAmount = 16;
     amountDue.textContent = startAmount;
+    sliderEl.value=1;
+    numberOfViews.textContent = 100;
+    sliderEl.style.background = 'hsl(224, 65%, 95%)';
     console.log(timeFrame, startAmount);
    }
 });
@@ -38,15 +46,15 @@ sliderEl.addEventListener("input", (event) => {
     sliderEl.style.background = `linear-gradient(to right, hsl(174, 77%, 80%) ${sliderValue}% , hsl(224, 65%, 95%) ${sliderValue}%  )`; 
     numberOfViews.textContent = 100 * sliderValue;
 
-    amountDue.textContent = startAmount * timeFrame * sliderValue;
+    amountDue.textContent =  startAmount * timeFrame * sliderValue;
     // if(!duration.checked) {
-    //     duration.textContent = "month";
-    //     amountDue.textContent = 16 * sliderValue;
+    //     // duration.textContent = "month";
+    //     // amountDue.textContent = 16 * sliderValue;
+    //     amountDue.textContent = startAmount * timeFrame * sliderValue;
     // } else {
-    //     duration.textContent ="year";
-    //     amountDue.textContent = 16 * 12 * sliderValue;
+    //     // duration.textContent ="year";
+    //     // amountDue.textContent = 16 * 12 * sliderValue;
+    //     amountDue.textContent = startAmount * timeFrame * sliderValue;
     // }
-
-
    
 })
